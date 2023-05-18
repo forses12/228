eebb187785e4656e79d0d672c399ffff2eadba28
vassi <asrtjlbv@ffgh.com>
2023-05-18 18:28:18 +0300

    d bhumijnk,l<Lkm,lj,jmhngb

--- view.py
+++ view.py
@@ -7,7 +7,7 @@ import model
 image1=pygame.image.load('лед 1.jpg')
 
 def draw():
-    pygame.transform.scale(image1,[60,screen.get_height()])
+    global image1
     a=math_utils.get_angle_by_point(model.small_rect.center, [model.small_rect.centerx + model.speed_x, model.small_rect.centery + model.speed_y])
     povorot=pygame.transform.rotate(model.image, a)
     big_rect=pygame.Rect([10,10],povorot.get_size())
@@ -27,16 +27,21 @@ def draw():
     fireball()
 
     screen.fill([0,0,0])
+    pygame.draw.rect(screen,[89,46,51],model.ice_rect)
     screen.blit(povorot, big_rect)
-    if model.ice_rect.w==60:
-        pygame.transform.rotate(image1,90)
+    if model.ice_rect.w==60 and image1.get_width()!=60:
+        image1=pygame.transform.rotate(image1,90)
+        image1=pygame.transform.scale(image1,[60,screen.get_height()])
         screen.blit(image1,[model.ice_rect.x,0])
-    else:
-        screen.blit(image1,[0,model.ice_rect.y])
+    elif model.ice_rect.w==60 and image1.get_width()==60:
+        screen.blit(image1,[model.ice_rect.x,0])
+    if model.ice_rect.h==60 and image1.get_height()!=60:
+        image1=pygame.transform.rotate(image1,90)
+        image1=pygame.transform.scale(image1,[screen.get_width(),60])
+        screen.blit(image1, [0, model.ice_rect.y])
+    elif model.ice_rect.h==60 and image1.get_height()==60:
+        screen.blit(image1, [0,model.ice_rect.y])
     # pygame.draw.rect(screen, [255,255,255], model.small_rect, 1)
     # pygame.draw.rect(screen,[255,255,255],big_rect,1)
-    pygame.draw.rect(screen,[89,46,51],model.ice_rect)
 
     pygame.display.flip()
-
-
